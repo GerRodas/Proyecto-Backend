3633df7f484e999618403003a9d673133b303960
German <germanrodas@gmail.com>
2023-02-01 18:51:04 -0300

    arreglos

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -5,12 +5,11 @@ import { createHash, isValidPassword } from "../utils.js";
 const LocalStrategy = local.Strategy
 
 const initializePassport = () => {
-    passport.use('register', new LocalStrategy({
-        {
+    passport.use('register', new LocalStrategy(
+        {        
             passReqToCallback: true, usernameField: 'email'
         },
         async (req, username, password, done)=>{
             const {first_name, last_name, email} =req.body
         }
-    }))
-}
+    ))}
